e45407c69452cf31da25264448dcec4d63ecdd68
ryosuke.sakuma <ryo.100per@gmail.com>
2020-07-16 06:39:36 +0000

    fourth commit

--- app/controllers/tasklists_controller.rb
+++ app/controllers/tasklists_controller.rb
@@ -1,2 +1,24 @@
-class TasklistsController < ApplicationController
+class TasklistController < ApplicationController
+  def index
+      @tasklist = Tasklist.all
+  end
+
+  def show
+      @tasklist = Tasklist.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
